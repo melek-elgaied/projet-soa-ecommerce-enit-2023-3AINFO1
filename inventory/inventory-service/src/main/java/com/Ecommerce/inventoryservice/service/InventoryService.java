d19ae8c8521ee9ea890a5d1ac1b1466121c752a2
Syrine Fekih <syrine.fekih@etudiant-enit.utm.tn>
2023-12-19 14:15:04 +0100

    - enregistrer la sortie d'une commande + enregistrer une reception d'un produit + check la quantité en stock disponible

--- inventory/inventory-service/src/main/java/com/Ecommerce/inventoryservice/service/InventoryService.java
+++ inventory/inventory-service/src/main/java/com/Ecommerce/inventoryservice/service/InventoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class InventoryService {
@@ -16,7 +17,7 @@ public class InventoryService {
     public InventoryService(InventoryRepository inventoryRepository) {
         this.inventoryRepository = inventoryRepository;
     }
-@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     public boolean isInStock(String productName){
        return inventoryRepository.findByproductName(productName).isPresent();
 
@@ -35,4 +36,37 @@ public class InventoryService {
     }
     public void  deleteIventory(Long id){
         inventoryRepository.deleteById(id);}
+
+    public Optional<Integer> getProductAvailableQuantity(Long id) {
+        Optional<Inventory> inventory = inventoryRepository.findById(id);
+        if (inventory.isPresent()) {
+            return Optional.of(inventory.get().getStockQuantity());
+        }
+        return Optional.of(0);
+    }
+    @Transactional
+    public boolean enregistrerSortieCommande(Long productId, int quantityToDeduct) {
+        Optional<Inventory> inventoryOpt = inventoryRepository.findById(productId);
+        if (inventoryOpt.isPresent()) {
+            Inventory inventory = inventoryOpt.get();
+            int newReservedQuantity = inventory.getReservedQuantity() - quantityToDeduct;
+            if (newReservedQuantity >= 0) {
+                inventory.setReservedQuantity(newReservedQuantity);
+                inventoryRepository.save(inventory);
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean updateQuantityProduct(Long productId, int addedQuantity) {
+        Optional<Inventory> inventoryOpt = inventoryRepository.findById(productId);
+        if (inventoryOpt.isPresent()) {
+            Inventory inventory = inventoryOpt.get();
+            int newQuantity = inventory.getStockQuantity() + addedQuantity;
+            inventory.setStockQuantity(newQuantity);
+            inventoryRepository.save(inventory);
+            return true;
+        }
+        return false;
+    }
 }

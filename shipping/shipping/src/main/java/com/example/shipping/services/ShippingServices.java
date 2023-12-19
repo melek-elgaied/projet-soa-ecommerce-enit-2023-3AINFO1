5a614d1505375c34fd4a492c6ca66e19adde2199
Fadwa Marzouk <fadoua.marzouk@mass-analytics.com>
2023-12-20 00:13:33 +0100

    Modefications to model and services

--- shipping/shipping/src/main/java/com/example/shipping/services/ShippingServices.java
+++ shipping/shipping/src/main/java/com/example/shipping/services/ShippingServices.java
@@ -1,18 +1,32 @@
 package com.example.shipping.services;
 
+import com.example.shipping.repository.ShippingRepository;
+import com.example.shipping.shipping.ShipmentStatus;
+import com.example.shipping.shipping.Shipping;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 
 public class ShippingServices {
+    private final ShippingRepository shippingRepo;
+    @Autowired
+    public ShippingServices(ShippingRepository shippingRepo) {
+        this.shippingRepo = shippingRepo;
+    }
 
     public static void shipmentByUserID(long userID) {
 
     }
-    public static void startShipment(){
+    public Shipping startShipment(Shipping shipping){
+
+
 
     }
-    public static void updateShipmentStatus(){
+    public Shipping updateShipmentStatus(Long idShipping, ShipmentStatus status){
 
     }
 }
--- shipping/shipping/src/main/java/com/example/shipping/shipping/Shipping.java
+++ shipping/shipping/src/main/java/com/example/shipping/shipping/Shipping.java
@@ -1,23 +1,27 @@
 package com.example.shipping.shipping;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
 public class Shipping {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
     private Long idShipping;
+
     private Long idUser;
     private String addressUser;
     private ShipmentStatus status;
 
 
-    public void setIdShipping(Long idShipping) {
-        this.idShipping = idShipping;
+    public Shipping(){
+        this.status=ShipmentStatus.Shipping;
     }
+
 
     @jakarta.persistence.Id
     public Long getIdShipping() {

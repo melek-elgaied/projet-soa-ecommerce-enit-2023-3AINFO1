c331f4707bb11220b12e6fb842b7f2818e3ff578
Azizlachtar <aziz.lachtar123@gmail.com>
2023-12-21 17:46:06 +0100

    Adding Lombok

--- search/search/src/main/java/com/SOA/search/model/Product.java
+++ search/search/src/main/java/com/SOA/search/model/Product.java
@@ -1,10 +1,14 @@
 package com.SOA.search.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Product {
 
     private Long id;
     private String name;
     private String description;
-
     private double price;
 }

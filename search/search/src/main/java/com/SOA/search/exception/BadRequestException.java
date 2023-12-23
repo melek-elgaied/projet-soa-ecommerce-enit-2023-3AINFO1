6d0e05c73ebc7e9ef3a1bd29156d98f90d9686bc
omar <romdaneo@gmail.com>
2023-12-23 16:40:14 +0100

    Corrected BadRequestException class constructor

--- search/search/src/main/java/com/SOA/search/exception/BadRequestException.java
+++ search/search/src/main/java/com/SOA/search/exception/BadRequestException.java
@@ -2,8 +2,15 @@ package com.SOA.search.exception;
 
 import java.io.IOException;
 
+/*
+ * Aziz lachtar keeps changing methods signatures without any prior conventions or agreements, based pn that the codded added within the same commit as this comment, as well as many others following this one, is just making sure methods signature are similar to the ones agreed on.
+ */
 public class BadRequestException extends ExceptionSoa{
     public BadRequestException(String message, IOException e) {
+        super(message);
+    }
+
+    public BadRequestException(String message) {
         super(message);
     }
 }
